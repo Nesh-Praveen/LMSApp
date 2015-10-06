b855e0102d468c281a790431576cfae66e50abcd
praveen <praveen@neshs-mac-mini.local>
2015-10-06 16:47:25 +0530

    Initial Commit

---
+++ LMSApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMSApp
+//
+//  Created by praveen on 06/10/15.
+//  Copyright © 2015 Nesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LMSAppTests/LMSAppTests.m
@@ -0,0 +1,39 @@
+//
+//  LMSAppTests.m
+//  LMSAppTests
+//
+//  Created by praveen on 06/10/15.
+//  Copyright © 2015 Nesh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LMSAppTests : XCTestCase
+
+@end
+
+@implementation LMSAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
